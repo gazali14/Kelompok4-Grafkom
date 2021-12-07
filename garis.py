82050de18333e275e1332874e0de471497c4cac0
gazali14 <77677262+gazali14@users.noreply.github.com>
2021-12-07 10:18:07 +0800

    Create garis.py

---
+++ garis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x1 = int(input('Masukan nilai x1  : '))
+y1 = int(input('Masukan nilai y1  : '))
+x2 = int(input('Masukan nilai x2  : '))
+y2 = int(input('Masukan nilai y2  : '))
+
+print('------------------------------------------------')
+
+nilaiY = y2 - y1
+nilaiX = x2 - x1
+N = x2 - x1 + 1
+
+x = x1
+y = y1
+
+i = 1
