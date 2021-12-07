b47abdf6bf4001780273600a6e2846bdf5b9bb09
gazali14 <77677262+gazali14@users.noreply.github.com>
2021-12-07 15:51:09 +0800

    Update garis.py

--- garis.py
+++ garis.py
@@ -14,7 +14,6 @@ N = x2 - x1 + 1
 
 x = x1
 y = y1
-
 i = 1
 
 if x1 == x2:
@@ -31,4 +30,40 @@ if x1 == x2:
       titikY.append(y+i)
     i+=1
   plt.plot(titikX,titikY)
+  plt.scatter(titikX,titikY)
+  plt.grid()
   plt.show()
+  
+elif y1 == y2:
+    titikX = []
+    titikY = []
+    while i < y2:
+        if x1 == x2:
+            print('Titik yang di lewati yaitu (',x,',',y,')')
+            titikX.append(x)
+            titikY.append(y)
+        else:
+            print('Titik yang di lewati yaitu (',x,',',y,')')
+            titikX.append(x+i)
+            titikY.append(y)
+        i+=1
+    plt.plot(titikX,titikY)
+    plt.scatter(titikX,titikY)
+    plt.grid()
+    plt.show()
+else:
+    titikX = []
+    titikY = []
+    while i <= N:
+        m = nilaiY / nilaiX
+        rumusY = m * (x - x1) + y1
+        kordinatY = round(rumusY)
+        x+=1
+        print('Titik yang di lewati yaitu (',x-1,',',kordinatY,')')
+        titikX.append(x-1)
+        titikY.append(kordinatY)
+        i+=1
+    plt.plot(titikX,titikY)
+    plt.scatter(titikX,titikY)
+    plt.grid()
+    plt.show()
